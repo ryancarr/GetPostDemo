ac07047fdb90b44884a2008ffc70607b1d2bd500
Ryan Carr <rycarr@gmail.com>
2019-07-16 18:36:32 -0500

    Turned server.js into an echo server
    Data sent via POST is echoed back to the webpage and displayed in
    an alert box.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post('/login',function(req,res){
   var user_name=req.body.user;
   var password=req.body.password;
   console.log("User name = "+user_name+", password is "+password);
-  res.end("yes");
+  res.send(JSON.stringify(req.body));
 });
 
 app.listen(3000,function(){
